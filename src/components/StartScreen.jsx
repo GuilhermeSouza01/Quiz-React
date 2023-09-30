9ade6ba5edad32654fe785054f47b4545b6cd9fd
guilhermesouza01 <ghostgui60@gmail.com>
2023-09-29 21:49:01 -0300

    first commit

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unescaped-entities */
+// import React from "react";
+
+const StartScreen = ({ numQuestions, dispatch }) => {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz</h2>
+      <h3>{numQuestions} questions to test your React Mastery</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's Start
+      </button>
+    </div>
+  );
+};
+
+export default StartScreen;
